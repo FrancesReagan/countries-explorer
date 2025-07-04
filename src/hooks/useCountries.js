5c905caa634f8e318efee617cae3905ba99f776b
Frances Reagan James <adtgwen@gmail.com>
2025-07-03 21:54:53 -0400

    added fetchCountries function with try catch error handling

--- src/hooks/useCountries.js
+++ src/hooks/useCountries.js
@@ -3,4 +3,33 @@ import { useState, useEffect } from "react";
 
 export const useCountries = () => {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+  
+  const [error, setError] = useState("");
+
+  const fetchCountries = async () => {
+    setLoading(true);
+    setError("");
+
+    try {
+      const response = await fetch("https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags,cca3");
+        if(!response.ok) {
+          throw new Error("Failed to fetch countries");
+        }
+
+        const data = await response.json();
+        setCountries(data);
+      }catch (error) {
+        setError("Failed to load countries...Try again.");
+  
+      } finally {
+        setLoading(false);
+      }
+    };
+
+
+    
+  }
+
+
 }
