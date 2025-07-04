7c1ea1b713772b95d6da44bfe79b45c61b3e1546
Frances Reagan James <adtgwen@gmail.com>
2025-07-04 13:13:47 -0400

    fixed typos in RegionFilter

--- src/components/RegionFilter.jsx
+++ src/components/RegionFilter.jsx
@@ -1,51 +1,51 @@
 // src/components/RegionFilter.jsx//
-import React from "react";
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import { useTheme } from "../context/ThemeContext";
 import './RegionFilter.css';
 
-const RegionFilter = ({selectedRegion, onRegionChange}) => {
-  const {isDark} = useTheme();
+const RegionFilter = ({ selectedRegion, onRegionChange }) => {
+  const { isDark } = useTheme();
   const [isOpen, setIsOpen] = useState(false);
-  const regions = ["Africa", "Americas", "Europe", "Oceania"];
+  const regions = ["Africa", "Americas", "Asia","Europe", "Oceania"];
 
   return (
    <div className="region-filter-container">
-    <button onClick={() => setIsOpen(!isOpen)}
-      className={`region-select ${isDark?'region-select-dark':'region-select-light'}`}
+    <button 
+      onClick={() => setIsOpen(!isOpen)}
+      className={`region-select ${isDark ? "region-select-dark":"region-select-light"}`}
       > 
-      {selectedRegion || "filter by region"}
+      {selectedRegion || "Filter by Region"}
       <ChevronDown size={16}/>
       </button>
 
-  {isOpen && (
-    <div className={`region-dropdown ${isDark?'region-dropdown-dark': 'region-dropdown-light'}`}>
-    <div className={`region-dropdown${isDark?"region-dropdown-dark" : "region-dropdown-light"}`}
-      <button 
-      className="region-option"
-      onClick={()=> {
-        onRegionChange("");
-    u    seIsOpen(false);
-      )}
-      >
-        All <Regions>
-    </div>
-          {regions.map(region => (
-            <div 
-            key={region}
+   {isOpen && (
+    <div className={`region-dropdown ${isDark ? "region-dropdown-dark" : "region-dropdown-light"}`}>
+        <button
+         className="region-option"
+         onClick={() => {
+          onRegionChange("");
+          setIsOpen(false);
+         }}
+         >
+        All Regions
+        </button> 
+    
+        {regions.map((region) => (
+            <button key={region}
+             className="region-option"
              onClick={()=> {
-              onRegionChange(region);
+               onRegionChange(region);
               setIsOpen(false);
-            }}
-            >
+            }}>
             {region}
-          </div>
+           </button>
           ))}
-        </div>
-        )}
-      </div>
-      );
-    };
+   </div>
+  )}
+ </div>
+  );
+};
+
 
     export default RegionFilter;
